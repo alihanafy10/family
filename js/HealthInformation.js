8fd4314e1a230bdf9abb21ff5935787d4844024b
alihanafy10 <113368324+alihanafy10@users.noreply.github.com>
2024-06-12 16:17:53 +0300

    Delete js/HealthInformation.js

--- js/HealthInformation.js
+++
@@ -1,6 +0,0 @@
-/**********  Health-information  *********/
-const nav = document.getElementById("nav");
-const HealthInformation = document.getElementById("HealthInformation");
-console.log(HealthInformation);
-HealthInformation.style.marginTop = nav.offsetHeight + "px";
-console.log(nav.offsetHeight);
