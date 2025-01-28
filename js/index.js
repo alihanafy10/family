f44fb1377a44c29c8c9e60ecb966b82e9dafda26
alihanafy10 <113368324+alihanafy10@users.noreply.github.com>
2025-01-28 21:07:59 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -129,184 +129,32 @@ pxSections.style.marginTop = nav.offsetHeight + "px";
 
 
 /*video*/
-// const players = document.querySelectorAll(".video-player");
-
-// players.forEach((player) => {
-//   new Plyr(player, {
-//     controls: [
-//       "play-large",
-//       "play",
-//       "progress",
-//       "current-time",
-//       "captions",
-//       "pip",
-//       "airplay",
-//       "fullscreen",
-//     ],
-//     youtube: {
-//       noCookie: true,
-//       rel: 0,
-//       showinfo: 0,
-//       iv_load_policy: 3,
-//     },
-//   });
-// });
-
-// const videoThum = document.querySelector(".video-thumbnail"); // صورة الفيديو
-// const videoModal = document.querySelector(".modeling"); // المودال
-// const videoFrame = videoModal.querySelector("iframe");
-
-// // وظيفة لفتح المودال عند النقر على الصورة المصغرة
-// videoThum.addEventListener("click", () => {
-//   if (videoModal.classList.contains("d-none")) {
-//     videoModal.classList.remove("d-none"); // إظهار المودال
-//   }
-// });
-
-// // إغلاق المودال عند النقر خارج محتوى المودال
-// window.addEventListener("click", (event) => {
-//   // تحقق إذا كان المودال مرئي
-//   if (!videoModal.classList.contains("d-none")) {
-//     const modalContent = videoModal.querySelector(".modal-content"); // محتوى المودال
-//     // تحقق إذا كان النقر خارج محتوى المودال بالكامل
-//     if (!modalContent.contains(event.target)) {
-//       console.log("Closing Modal");
-//       videoModal.classList.add("d-none"); // إخفاء المودال
-//       videoFrame.src = ""; // إزالة الرابط من iframe لإيقاف الفيديو
-//     }
-//   }
-// });
 
 
 
-
-
-
-
-
-
-  // const videoThumbnails = document.querySelectorAll('.video-thumbnail');
-
-  // // إظهار المودال عند الضغط على الصورة المصغرة
-  // videoThumbnails.forEach((thumbnail) => {
-  //   thumbnail.addEventListener('click', () => {
-  //     // العثور على المودال المناسب
-  //     const modal = thumbnail.closest('.col-lg-3').querySelector('.modeling');
-  //     const iframe = modal.querySelector('iframe');
-  //     const tiktokEmbed = modal.querySelector('blockquote');
-  
-  //     // إظهار المودال
-  //     modal.classList.remove('d-none');
-  
-  //     // إذا كان هناك iframe، قم بتحديث src
-  //     if (iframe) {
-  //       iframe.src = iframe.src; // إعادة تحميل iframe لإيقاف الفيديو
-  //     }
-  
-  //     // إعادة تحميل فيديو تيك توك إذا كان موجودًا
-  //     if (tiktokEmbed) {
-  //       const newEmbed = tiktokEmbed.cloneNode(true);
-  //       tiktokEmbed.parentNode.replaceChild(newEmbed, tiktokEmbed);
-  //       // إعادة تحميل النص البرمجي الخاص بتيك توك
-  //       const script = document.createElement('script');
-  //       script.src = 'https://www.tiktok.com/embed.js';
-  //       document.body.appendChild(script);
-  //     }
-  //   });
-  // });
-  
-  // // إغلاق المودال عند الضغط خارج الفيديو
-  // document.querySelectorAll('.modeling').forEach((modal) => {
-  //   modal.addEventListener('click', (event) => {
-  //     if (!event.target.closest('.modal-content')) {
-  //       modal.classList.add('d-none');
-  //       // إعادة تعيين src لإيقاف الفيديو
-  //       const iframe = modal.querySelector('iframe');
-  //       if (iframe) {
-  //         iframe.src = iframe.src; // إيقاف الفيديو
-  //       }
-  //     }
-  //   });
-  // });
-  
-
-
-// الحصول على العناصر الضرورية
-// const videoThumbnails = document.querySelectorAll('.video-thumbnail');
-// const modals = document.querySelectorAll('.modeling');
-
-// // إظهار المودال عند الضغط على الصورة المصغرة
-// videoThumbnails.forEach((thumbnail) => {
-//   thumbnail.addEventListener('click', () => {
-//     const modal = thumbnail.nextElementSibling; // العثور على المودال الخاص بالصورة المصغرة
-//     const iframe = modal.querySelector('iframe');
-
-//     // قراءة رابط الفيديو من data-video-url (إذا كان موجودًا)
-//     const videoUrl = thumbnail.getAttribute('data-video-url');
-//     if (videoUrl) {
-//       iframe.src = videoUrl; // تعيين الرابط إلى iframe
-//     }
-
-//     // إظهار المودال
-//     modal.classList.remove('d-none');
-//   });
-// });
-
-// // إغلاق المودال عند الضغط على زر الإغلاق أو خارج المحتوى
-// modals.forEach((modal) => {
-//   const closeButton = modal.querySelector('.close-modal'); // زر الإغلاق
-
-//   // عند الضغط خارج الفيديو
-//   modal.addEventListener('click', (event) => {
-//     if (!event.target.closest('.modal-content')) {
-//       closeModal(modal);
-//     }
-//   });
-
-//   // عند الضغط على زر الإغلاق
-//   closeButton.addEventListener('click', () => {
-//     closeModal(modal);
-//   });
-// });
-
-// // دالة لإغلاق المودال
-// function closeModal(modal) {
-//   modal.classList.add('d-none'); // إخفاء المودال
-//   const iframe = modal.querySelector('iframe');
-//   if (iframe) iframe.src = ""; // إعادة تعيين src لإيقاف الفيديو
-// }
-
-
-
-
-
-
-
-
-// الحصول على العناصر الضرورية
 const videoThumbnails = document.querySelectorAll('.video-thumbnail');
 
-// إظهار المودال عند الضغط على الصورة المصغرة
+
 videoThumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener('click', () => {
-    // العثور على المودال المناسب
+   
     const modal = thumbnail.closest('.col-lg-3').querySelector('.modeling');
     const iframe = modal.querySelector('iframe');
     const tiktokEmbed = modal.querySelector('blockquote');
 
-    // إظهار المودال
+   
     modal.classList.remove('d-none');
 
-    // إذا كان هناك iframe، قم بتحديث src
+  
     if (iframe) {
       iframe.src = iframe.src; // إعادة تحميل iframe لإيقاف الفيديو
     }
 
-    // إعادة تحميل فيديو تيك توك إذا كان موجودًا
+    
     if (tiktokEmbed) {
       const newEmbed = tiktokEmbed.cloneNode(true);
       tiktokEmbed.parentNode.replaceChild(newEmbed, tiktokEmbed);
-      // إعادة تحميل النص البرمجي الخاص بتيك توك
+      
       const script = document.createElement('script');
       script.src = 'https://www.tiktok.com/embed.js';
       document.body.appendChild(script);
